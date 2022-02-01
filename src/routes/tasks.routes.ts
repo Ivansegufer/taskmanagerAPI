b1d2257a2ec45d31ea8386032df105976e312cf2
Ivan Guzman <social@ivansegufer.com>
2022-01-31 19:57:06 -0500

    Validate Resource middleware has been agregated to task route '/' method POST

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -6,10 +6,15 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/tasks";
+import validateResource from "../middlewares/validateResource";
+import { createTaskSchema } from "../validations/task.schema";
 
 const router = Router();
 
-router.route("/").get(getAllTasks).post(createTask);
+router
+  .route("/")
+  .get(getAllTasks)
+  .post(validateResource(createTaskSchema), createTask);
 router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 export default router;
