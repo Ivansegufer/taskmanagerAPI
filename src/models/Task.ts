0ca7b12af73132055627f93f12e1df8dd4291f0e
Ivan Guzman <social@ivansegufer.com>
2022-01-31 21:40:51 -0500

    Refactor models

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,10 +1,15 @@
 import { Document, model } from "mongoose";
 import TaskSchema from "../schemas/TaskSchema";
 
-export interface TaskDocument extends Document {
+export interface TaskInput {
   name: string;
   description: string;
   completed: boolean;
+}
+
+export interface TaskDocument extends TaskInput, Document {
+  createdAt: Date;
+  updateAt: Date;
 }
 
 const TaskModel = model<TaskDocument>("Task", TaskSchema);
